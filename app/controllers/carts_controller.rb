4735b09194096fd17fe42aa9247a7baa0778e809
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-19 12:06:51 -0500

    Refactor carts_controller#checkout to mark carts as submitted. About to merge with master

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,16 +2,16 @@ class CartsController < ApplicationController
 
 
   def show
-
     if !active_cart
         flash[:notice] = "Your cart is empty."
     end
-
   end
 
   def checkout
     subtract_inventory
-    current_user.current_cart = nil
+    active_cart.status = "submitted"
+    active_cart.save
+    current_user.current_cart_id = nil
     current_user.save
     flash[:notice] = "Your cart is empty."
     redirect_to cart_path(params[:id])
